960998bc0252462fd3f30acd76fdd49140fe6b26
Aimee <aimeehorwood@hotmail.com>
2023-01-10 14:34:21 +0000

    adds hook to output blog posts

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+
+        { title: 'My chicken soup recipe ', body: 'lorem ipsum...', author: 'mary', id: 1 },
+        { title: 'Welcome Home!', body: 'lorem ipsum...', author: 'zara', id: 2 },
+        { title: 'Ways to brush your cat', body: 'lorem ipsum...', author: 'mary', id: 3 }
+    ])
+
     return ( 
         <div className="home">
-            <h2> Homepage </h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2> {blog.title}</h2>
+                    <p> Written by {blog.author}</p>
+                </div>
+
+            ))}
+
+            
+
+
         </div>
      );
 }
