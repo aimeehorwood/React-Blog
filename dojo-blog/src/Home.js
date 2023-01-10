0fc955a0e7ecf232f001dfa7b4fcccb52845755d
Aimee <aimeehorwood@hotmail.com>
2023-01-10 14:59:09 +0000

    adds prop for blog posts

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,28 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My chicken soup recipe ",
+      body: "lorem ipsum...",
+      author: "mary",
+      id: 1,
+    },
+    { title: "Welcome Home!", body: "lorem ipsum...", author: "zara", id: 2 },
+    {
+      title: "Ways to brush your cat",
+      body: "lorem ipsum...",
+      author: "mary",
+      id: 3,
+    },
+  ]);
 
-        { title: 'My chicken soup recipe ', body: 'lorem ipsum...', author: 'mary', id: 1 },
-        { title: 'Welcome Home!', body: 'lorem ipsum...', author: 'zara', id: 2 },
-        { title: 'Ways to brush your cat', body: 'lorem ipsum...', author: 'mary', id: 3 }
-    ])
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" />
+    </div>
+  );
+};
 
-    return ( 
-        <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2> {blog.title}</h2>
-                    <p> Written by {blog.author}</p>
-                </div>
-
-            ))}
-
-            
-
-
-        </div>
-     );
-}
- 
 export default Home;
