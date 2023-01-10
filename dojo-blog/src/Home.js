ebf6f15d77d4286a7d331fb17b932169b786efa0
Aimee <aimeehorwood@hotmail.com>
2023-01-10 15:20:17 +0000

    pass functions into other components as props

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -18,11 +18,15 @@ const Home = () => {
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+    }
+  
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" />
-      <BlogList blogs={blogs.filter((blog)=> blog.author === 'mary' 
-        )} title="Mary's blogs" />
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
     </div>
   );
 };
